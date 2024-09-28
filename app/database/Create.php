d2b4b15834223bdb2bd17d00aa926efed932566b
Benjamin <aeplibenjamin@gmail.com>
2024-09-28 11:18:45 +0200

    finish db

--- app/database/Create.php
+++ app/database/Create.php
@@ -4,36 +4,45 @@ require_once 'Connection.php';
 use app\database\Connection;
 $conn = Connection::getInstance()->getConnection();
 
-
 $userQuery = "CREATE TABLE IF NOT EXISTS users (
-    user_id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     surname VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
     city VARCHAR(50),
-    postal_code int(10),
+    postal_code INT(10),
     street VARCHAR(30),
-    house_number int(10)
+    house_number INT(10)
 )";
 
 $receiptQuery = "CREATE TABLE IF NOT EXISTS receipts (
-    receipt_id INT(6) AUTO_INCREMENT PRIMARY KEY,
-    user_id INT(6) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    receipt_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     orderdate DATE NOT NULL,
-   price DECIMAL(10,2) NOT NULL
+    price DECIMAL(10,2) NOT NULL
 )";
 
 $orderQuery = "CREATE TABLE IF NOT EXISTS orders (
-    receipt_id INT(6) NOT NULL,
-    FOREIGN KEY (receipt_id) REFERENCES receipts(id),
-    pizza_id INT(6) NOT NULL,
-    FOREIGN KEY (pizza_id) REFERENCES products(id)
+    receipt_id INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (receipt_id) REFERENCES receipts(receipt_id),
+    pizza_id INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (pizza_id) REFERENCES pizza(pizza_id)
 )";
 
 $pizzaQuery = "CREATE TABLE IF NOT EXISTS pizza (
-    pizza_id INT(6) AUTO_INCREMENT PRIMARY KEY,
-    price DECIMAL(10,2) NOT NULL.
-    toppings int(10) NOT NULL,
+    pizza_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    price DECIMAL(10,2) NOT NULL,
+    toppings INT(10) NOT NULL,
     message VARCHAR(1000)
 )";
+
+$queries = array($userQuery, $receiptQuery, $orderQuery, $pizzaQuery);
+
+foreach ($queries as $sql) {
+    if ($conn->query($sql) === TRUE) {
+        echo "table"+ $sql + "created successfully";
+    } else {
+        echo "error creating table "+ $sql + ": " . $conn->error;
+    }
+}
