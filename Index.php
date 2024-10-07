f064025ebab41d9d6fd39c3ee3660603aa94158f
Benjamin <aeplibenjamin@gmail.com>
2024-10-07 14:58:56 +0200

    remove manual autoload

--- Index.php
+++ Index.php
@@ -1,25 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 require_once('Routes.php');
 
 $request = $_SERVER['REQUEST_URI'];
 $request = str_replace('/Pizzaservice/', '', $request);
 Route::dispatch($request);
 
-spl_autoload_register(function ($class_name) {
-    $base_dir = __DIR__ . '/app/';
-    $file = $base_dir . str_replace('\\', '/', $class_name) . '.php';
-
-    if (file_exists($file)) {
-        require_once($file);
-    } else {
-        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($base_dir));
-        foreach ($iterator as $fileInfo) {
-            if ($fileInfo->isFile() && $fileInfo->getFilename() === $class_name . '.php') {
-                require_once($fileInfo->getPathname());
-                return;
-            }
-        }
-        error_log("File not found: " . $class_name . '.php');
-    }
-});
