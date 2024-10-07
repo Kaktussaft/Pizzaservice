c36833a95d049132dacb7beb4a7bd22b70bcafcb
Benjamin <aeplibenjamin@gmail.com>
2024-10-07 10:51:35 +0200

    db access

---
+++ app/Repository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app; 
+use app\database\Connection;
+
+class Repository
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = Connection::getInstance()->getConnection();
+    }
+
+    public function ExecuteQuery($query, string $parameterTypes, $parameters)
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param($parameterTypes, $parameters);
+        $stmt->execute();
+        $stmt->close();
+        return $stmt;
+    }
+}
+
