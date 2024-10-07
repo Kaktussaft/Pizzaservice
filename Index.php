be63d8dccc5794a4168d00051b2a20d307a1900d
Benjamin <aeplibenjamin@gmail.com>
2024-10-07 10:51:27 +0200

    check subdirectories

--- Index.php
+++ Index.php
@@ -7,11 +7,19 @@ $request = str_replace('/Pizzaservice/', '', $request);
 Route::dispatch($request);
 
 spl_autoload_register(function ($class_name) {
-    $file = 'app/' . $class_name . '.php';
+    $base_dir = __DIR__ . '/app/';
+    $file = $base_dir . str_replace('\\', '/', $class_name) . '.php';
+
     if (file_exists($file)) {
         require_once($file);
     } else {
-        error_log("File not found: " . $file);
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($base_dir));
+        foreach ($iterator as $fileInfo) {
+            if ($fileInfo->isFile() && $fileInfo->getFilename() === $class_name . '.php') {
+                require_once($fileInfo->getPathname());
+                return;
+            }
+        }
+        error_log("File not found: " . $class_name . '.php');
     }
 });
-?>
