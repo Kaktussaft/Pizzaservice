dd9576f6cae1e9f8d07161a5094b60fbbb5551fb
Benjamin <aeplibenjamin@gmail.com>
2024-10-07 09:24:36 +0200

    add receipt

--- app/views/Receipt.php
+++ app/views/Receipt.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/Pizzaservice/app/style/Shared.css">
@@ -7,12 +8,25 @@
     <script src="/Pizzaservice/app/shared.js"></script>
     <title>Receipt</title>
 </head>
+
 <body>
-<div class="navigation">
+    <div class="navigation">
         <button class="navigation-button" type="submit">Hauptmenü</button>
     </div>
-    
 
-    
+    <div class="receipt-box">
+        <br><br>
+        <div>
+        <h1>Rechnungen</h1><br>
+        </div class="receipt-container">
+        <div><select name="rechnungen" id="rechnungen" class="receipt-select">
+                <option value="rechnung1">Rechnung 1</option>
+                <option value="rechnung2">Rechnung 2</option>
+                <option value="rechnung3">Rechnung 3</option>
+        </div>
+
+    </div>
+
 </body>
+
 </html>
