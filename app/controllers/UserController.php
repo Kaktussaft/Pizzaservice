72fc5941eb22cfde68e5f76f1160f20b3f7e86c5
Benjamin <aeplibenjamin@gmail.com>
2024-10-08 11:36:21 +0200

    nav + register

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,12 +3,49 @@
 namespace app\controllers;
 
 
-use app\builders\UserQueries;
+use  app\queries\UserQueries;
 
 class UserController
 {
+    private $userQueries;
+
+    public function __construct()
+    {
+        $this->userQueries = new UserQueries();
+    }
+
     public function redirectToMainMenu()
     {
-        header("http://localhost/Pizzaservice/Orderpage");
+        header("Location: http://localhost/Pizzaservice/Orderpage");
+        exit();
     }
+
+    public function redirectToReceipt()
+    {
+        header("Location: http://localhost/Pizzaservice/Receipt");
+        exit();
+    }
+
+    public function Logout()
+    {
+        session_destroy();
+        header("Location: http://localhost/Pizzaservice/Login");
+        exit();
+    }
+
+    public function Login()
+    {
+
+    }
+
+    public function register($name, $surname, $password, $city, $postal_code, $street, $house_number)
+    {
+        if($this->userQueries->uniqueUser($name, $password) != null)
+        {
+            return "User already exists";
+        }
+        $result = $this->userQueries->CreateUser($name, $surname, $password, $city, $postal_code, $street, $house_number);
+        return $result;
+    }
+   
 }
