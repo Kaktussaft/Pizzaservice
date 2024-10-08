fb62f1fe2a5d2a22207ffbb606ddc78d5f3611c9
Benjamin <aeplibenjamin@gmail.com>
2024-10-08 11:36:33 +0200

    create + check user

--- app/queries/UserQueries.php
+++ app/queries/UserQueries.php
@@ -14,6 +14,25 @@ class UserQueries
 
     public function __construct()
     {
-        $this->repository = new repository\Repository();
+        $this->repository = new Repository();
     }
+
+    public function createUser($name, $surname, $password, $city, $postal_code, $street, $house_number)
+    {
+        $sql = $this->insert . " (name, surname, password, city, postal_code, street, house_number) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $parametertypes = "ssssisi";
+        $parameters = array($name, $surname, $password, $city, $postal_code, $street, $house_number);
+        $result = $this->repository->ExecuteQuery($sql, $parametertypes, $parameters);
+        return $result;
+    }
+
+    public function uniqueUser($name, $password)
+    {
+        $sql = $this->select . " WHERE name = ? AND password = ?";
+        $parametertypes = "ss";
+        $parameters = array($name, $password);
+        $result = $this->repository->ExecuteQuery($sql, $parametertypes, $parameters);
+        return $result;
+    }
+
 }
