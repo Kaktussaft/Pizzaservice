42245343441fa2d24e05e623a00c935442d05d75
Benjamin <aeplibenjamin@gmail.com>
2024-10-08 16:31:24 +0200

    rem orderrs

--- app/database/Create.php
+++ app/database/Create.php
@@ -25,21 +25,15 @@ $receiptQuery = "CREATE TABLE IF NOT EXISTS receipts (
 
 $pizzaQuery = "CREATE TABLE IF NOT EXISTS pizza (
     pizza_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    receipt_id INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (receipt_id) REFERENCES receipts(receipt_id),
     price DECIMAL(10,2) NOT NULL,
     toppings INT(10) NOT NULL,
     message VARCHAR(1000)
 )";
 
-$orderQuery = "CREATE TABLE IF NOT EXISTS orders (
-    receipt_id INT(6) UNSIGNED NOT NULL,
-    FOREIGN KEY (receipt_id) REFERENCES receipts(receipt_id),
-    pizza_id INT(6) UNSIGNED NOT NULL,
-    FOREIGN KEY (pizza_id) REFERENCES pizza(pizza_id)
-)";
 
-
-
-$queries = array($userQuery, $receiptQuery, $pizzaQuery, $orderQuery);
+$queries = array($userQuery, $receiptQuery, $pizzaQuery);
 
 foreach ($queries as $sql) {
     if ($conn->query($sql) === TRUE) {
