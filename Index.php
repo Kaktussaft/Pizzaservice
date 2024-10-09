364c73ca15cf241e6877ed1c36c2ebc34361583d
Benjamin <aeplibenjamin@gmail.com>
2024-10-09 13:49:49 +0200

    start session in index

--- Index.php
+++ Index.php
@@ -3,6 +3,9 @@
 require __DIR__ . '/vendor/autoload.php';
 require_once('Routes.php');
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $request = $_SERVER['REQUEST_URI'];
 $request = str_replace('/Pizzaservice/', '', $request);
 Route::dispatch($request);
