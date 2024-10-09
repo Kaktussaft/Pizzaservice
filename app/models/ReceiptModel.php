fc93ef38282caf6f36171b6ef4b873d74e59ec2e
Benjamin <aeplibenjamin@gmail.com>
2024-10-09 09:33:39 +0200

    fk

--- app/models/ReceiptModel.php
+++ app/models/ReceiptModel.php
@@ -8,12 +8,14 @@ class ReceipModel
 {
     public int $id;
     public int $userId;
+    public int $pizzaId;
     public DateTime $orderDate;
     public float $totalPrice;
 
-    public function __construct(int $userId, DateTime $orderDate, float $totalPrice)
+    public function __construct(int $userId, int $pizzaId, DateTime $orderDate, float $totalPrice)
     {
         $this->userId = $userId;
+        $this->pizzaId = $pizzaId;
         $this->orderDate = $orderDate;
         $this->totalPrice = $totalPrice;
     }
