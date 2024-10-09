4b1158234360bed5fd7d3bdacda21ad762fe8cba
Benjamin <aeplibenjamin@gmail.com>
2024-10-09 09:33:47 +0200

    create pizza

--- app/queries/PizzaQueries.php
+++ app/queries/PizzaQueries.php
@@ -3,6 +3,8 @@
 namespace app\queries;
 
 use app\repository; 
+use app\models\Pizza;
+use app\models\PizzaModel;
 
 class PizzaQueries
 {
@@ -11,9 +13,20 @@ class PizzaQueries
     private $delete = "DELETE FROM Pizza";
     private $insert = "INSERT INTO Pizza";
     private $repository;
+    private $pizzaModel;
 
     public function __construct()
     {
         $this->repository = new Repository();
     }
+
+    public function create(PizzaModel $pizza)
+    {
+        $sql = $this->insert . " (price, toppings, message) VALUES (?, ?, ?)";
+        $parametertypes = "iis";
+        $parameters = array($pizza->price, $pizza->toppings, $pizza->message);
+        $result = $this->repository->ExecuteQuery($sql, $parametertypes, $parameters);
+        return $result;
+    }
+
 }
